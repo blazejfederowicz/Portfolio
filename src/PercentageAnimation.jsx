9eafe30145ef4e46324778d35a33bc2524621715
Błażej <wargun364@gmail.com>
2025-03-22 16:06:10 +0100

    working percentage

--- src/PercentageAnimation.jsx
+++ src/PercentageAnimation.jsx
@@ -1,40 +1,45 @@
 import { useState, useEffect, useRef } from "react";
 
 export default function PercentageAnimation({first, second, duration,rerender}){
-    const [percentage, setPercentage] = useState(first)
+    const [percentage, setPercentage] = useState(second)
+    const [target, setTarget] = useState(first)
     const intervalId = useRef(null);
     const hasMounted = useRef(false)
     let calcDuration = Math.max(duration/Math.abs(first-second),1)
 
     useEffect(()=>{
-        const handleProcentageAnim = (firstNum,secondNum)=>{
-            let min = Math.min(firstNum, secondNum)
-            let max = Math.max(firstNum, secondNum)
-            
-            if( secondNum > percentage ){
-                let i = min;
-                intervalId.current=setInterval(()=>{
-                    i++;
-                    if(i>max){
-                        clearInterval(intervalId.current)
-                    }else{
-                        setPercentage(i)
-                    }
-                },calcDuration);
-            } else{
-                let i = max;
-                intervalId.current=setInterval(()=>{
-                    i--;
-                    if(i<min){
-                        clearInterval(intervalId.current)
-                    }else{
-                        setPercentage(i)
-                    }
-                },calcDuration);
+        const handleProcentageAnim = (firstNum, secondNum) => {
+            if (intervalId.current) clearInterval(intervalId.current);
+          
+            let min = Math.min(secondNum,firstNum)
+            let max = Math.max(secondNum,firstNum)
+            const newTarget = target ===second?first:second;
+            const shouldIncrement = target < newTarget;
+            console.log(`target: ${target}\nnewTarget: ${newTarget}\nshould Increment: ${shouldIncrement}`)
+            setTarget(newTarget)
+          
+            if (shouldIncrement) {
+              let i = min;
+              intervalId.current = setInterval(() => {
+                i++;
+                if (i > max) {
+                  clearInterval(intervalId.current);
+                } else {
+                  setPercentage(i);
+                }
+              }, calcDuration);
+            } else {
+              let i = max;
+              intervalId.current = setInterval(() => {
+                i--;
+                if (i <= min) {
+                  clearInterval(intervalId.current);
+                } else {
+                  setPercentage(i);
+                }
+              }, calcDuration);
             }
-            
-           
-        }
+          };
 
         if(hasMounted.current){
             handleProcentageAnim(first,second);
