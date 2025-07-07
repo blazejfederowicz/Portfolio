7abb028d54bbe95e49c8813140d75d1e8396c2f0
Błażej <wargun364@gmail.com>
2025-07-07 16:10:07 +0200

    added new expiriance

--- src/components/ExpirianceComponent.jsx
+++ src/components/ExpirianceComponent.jsx
@@ -15,6 +15,31 @@ const ExpirianceComponent = ()=>{
                                 <div className="rounded-lg border border-zinc-800/80 border-r-4 bg-indigo-900/15 text-gray-200 shadow-sm relative overflow-hidden">
                                     <div className="p-6">
                                         <div className="mb-2 flex flex-wrap gap-x1 justify-between items-start">
+                                            <h4 className="font-medium text-base sm:text-lg">Outlier (Freelance via RLFH)</h4>
+                                            <span className="text-xs sm:text-sm text-indigo-100/60">May 2025 - Present</span>
+                                        </div>
+                                        <p className="text-indigo-100/60 mb-4 text-xs sm:text-sm">AI Training & Specialist
+                                        </p>
+                                        <ul className="space-y-2 pl-5 text-xs sm:text-sm list-disc  ">
+                                            <li className="opacity-80">
+                                                Reviewed and corrected AI-generated JavaScript code outputs.
+                                            </li>
+                                            <li className="opacity-80">
+                                                Contributed to LLM fine-tuning through technical feedback and
+                                                training data generation.
+                                            </li>
+                                            <li className="opacity-80">
+                                                Ensured a 95%+ accuracy rate in evaluated outputs across
+                                                various difficulty levels.
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </Reveal>
+                            <Reveal>
+                                <div className="rounded-lg border border-r-4 border-indigo-950 bg-black text-gray-200 shadow-sm relative overflow-hidden group hover:border-indigo-500/50 transition-colors duration-300">
+                                    <div className="p-6">
+                                        <div className="mb-2 flex flex-wrap gap-x1 justify-between items-start">
                                             <h4 className="font-medium text-base sm:text-lg">Computer Technician Intern</h4>
                                             <span className="text-xs sm:text-sm text-indigo-100/60">May 2022 - June 2022</span>
                                         </div>
@@ -26,8 +51,6 @@ const ExpirianceComponent = ()=>{
                                                 Installing, configuring, and troubleshooting printers, including both wired and wireless models.</li>
                                             <li className="opacity-80">
                                                 Servicing, Repair, and Operational Management of Computer Hardware and Peripherals</li>
-                                            <li className="opacity-80">
-                                                Documentation & Reporting.</li>
                                         </ul>
                                     </div>
                                 </div>
@@ -37,7 +60,7 @@ const ExpirianceComponent = ()=>{
                                     <div className="p-6">
                                         <div className="mb-2 flex flex-wrap gap-x1 justify-between items-start">
                                             <h4 className="font-medium text-base sm:text-lg">Game Dev Intern</h4>
-                                            <span className="text-xs sm:text-sm text-indigo-100/60">Oct 2023 - Dec 2023</span>
+                                            <span className="text-xs sm:text-sm text-indigo-100/60">Nov 2022 - Dec 2022</span>
                                         </div>
                                         <p className="text-indigo-100/60 mb-4 text-xs sm:text-sm">Art of Code</p>
                                         <ul className="space-y-2 relative list-disc ml-5 text-xs sm:text-sm">
@@ -55,8 +78,40 @@ const ExpirianceComponent = ()=>{
                             <div data-orientation="horizontal" role="none" className="shrink-0 h-[1px] w-full mt-4 max-w-[100px] bg-blue-300/30"></div>
                             <div data-orientation="horizontal" role="none" className="shrink-0 h-[1px] w-full mt-2 mb-6 lg:mb-10 max-w-[50px] bg-blue-300/30"></div>
                         </Reveal>
-                        <Reveal custom="overflow-hidden">
+                        <Reveal>
                             <div className="rounded-lg border border-zinc-800/80 border-r-4 bg-indigo-900/15 text-gray-200 shadow-sm relative overflow-hidden">
+                                <div className="p-6">
+                                    <div className="mb-2 flex flex-wrap gap-x1 justify-between items-start">
+                                        <h4 className="font-medium text-base sm:text-lg">Epam</h4>
+                                        <span className="text-xs sm:text-sm text-indigo-100/60">Jun 2025 - Present</span>
+                                    </div>
+                                    <p className="text-indigo-100/60 mb-4 text-xs sm:text-sm">Trainee Front-End Developer
+                                    </p>
+                                    <ul className="space-y-2 pl-5 text-xs sm:text-sm list-disc  ">
+                                        <li className="opacity-80">
+                                            Built a scalable web application using React, TypeScript, Redux,
+                                            and Axios, reducing load time by 35%.
+                                        </li>
+                                        <li className="opacity-80">
+                                            Applied modular coding practices, improving code reuse by 40%
+                                            across the app.
+                                        </li>
+                                        <li className="opacity-80">
+                                            Participated in code reviews and version control with Git.
+                                        </li>
+                                        <li className="opacity-80">
+                                            Practiced unit testing with Jest and React Testing Library.
+                                        </li>
+                                        <li className="opacity-80">
+                                            Learned corporate-grade workflows through hands-on mentorship
+                                            and real cycles
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </Reveal>
+                        <Reveal custom="overflow-hidden">
+                            <div className="rounded-lg border border-r-4 border-indigo-950 bg-black text-gray-200 shadow-sm relative overflow-hidden group hover:border-indigo-500/50 transition-colors duration-300">
                                 <div className="p-6">
                                     <div className="mb-2 flex flex-wrap gap-x1 justify-between items-start">
                                         <h4 className="font-medium text-base sm:text-lg">IT Technician</h4>
